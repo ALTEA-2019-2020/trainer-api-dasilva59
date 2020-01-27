47a89a8349184cd372fd3f087a0df61aea0bf18f
elsa1dasilva <59056006+elsa1dasilva@users.noreply.github.com>
2020-01-27 10:51:02 +0100

    ADD

---
+++ src/main/java/com/miage/altea/trainer_api/service/TrainerService.java
@@ -0,0 +1,12 @@
+package com.miage.altea.trainer_api.service;
+
+import com.miage.altea.trainer_api.bo.Trainer;
+import org.springframework.stereotype.Service;
+
+
+public interface TrainerService {
+
+    Iterable<Trainer> getAllTrainers();
+    Trainer getTrainer(String name);
+    Trainer createTrainer(Trainer trainer);
+}
---
+++ src/main/java/com/miage/altea/trainer_api/service/TrainerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.miage.altea.trainer_api.service;
+
+import com.miage.altea.trainer_api.bo.Trainer;
+import com.miage.altea.trainer_api.repository.TrainerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrainerServiceImpl implements TrainerService {
+
+    private TrainerRepository trainerRepository;
+
+    public TrainerServiceImpl(TrainerRepository trainerRepository) {
+        this.trainerRepository = trainerRepository;
+    }
+
+    @Override
+    public Iterable<Trainer> getAllTrainers() {
+        return this.trainerRepository.findAll();
+    }
+
+    @Override
+    public Trainer getTrainer(String name)  {
+        if(this.trainerRepository.findById(name).isPresent())
+             return this.trainerRepository.findById(name).get();
+        else
+            return null;
+    }
+
+    @Override
+    public Trainer createTrainer(Trainer trainer) {
+        return this.trainerRepository.save(trainer);
+    }
+}
